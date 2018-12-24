2a3ba69e4e27e083bfdab4c548ea7f04eec87954
ENAM <russell.m2006@gmail.com>
2018-12-24 01:17:21 -0500

    theory question's answer

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.ArrayList;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,7 +11,38 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+
+		ArrayList<Integer> arrlist = new ArrayList<Integer>();
+		arrlist.add(14);
+		arrlist.add(7);
+		arrlist.add(39);
+		arrlist.add(40);
+		// remove element from arraylist
+		arrlist.remove(14);
+		// remove element from arraylist
+		arrlist.remove(7);
+		// get index
+		arrlist.get(2);
+
+		/* For Loop for iterating ArrayList */
+		System.out.println("For Loop");
+		for (int counter = 0; counter < arrlist.size(); counter++) {
+			System.out.println(arrlist.get(counter));
+		}
+
+		/* Advanced For Loop*/
+		System.out.println("Advanced For Loop");
+		for (Integer num : arrlist) {
+			System.out.println(num);
+		}
+
+		/* While Loop for iterating ArrayList*/
+		System.out.println("While Loop");
+		int count = 0;
+		while (arrlist.size() > count) {
+			System.out.println(arrlist.get(count));
+			count++;
+		}
 
 	}
 
--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,5 +1,7 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class FindLowestDifference {
     public static void main(String[] args) {
         /*
@@ -10,10 +12,39 @@ public class FindLowestDifference {
         int [] array1 = {30,12,5,9,2,20,33,1};
         int [] array2 = {18,25,41,47,17,36,14,19};
 
-        int x = (array2.length)-(array1.length);
-        System.out.println(x);
 
 
+        static int findSmallestDifference(int A[], int B[],
+        int m, int n)
+        {
+            // Sort both arrays
+            // using sort function
+            Arrays.sort(A);
+            Arrays.sort(B);
+
+            int a = 0, b = 0;
+
+            // Initialize result as max value
+            int result = Integer.MAX_VALUE;
+
+            // Scan Both Arrays upto
+            // sizeof of the Arrays
+            while (a < m && b < n)
+            {
+                if (Math.abs(A[a] - B[b]) < result)
+                    result = Math.abs(A[a] - B[b]);
+
+                // Move Smaller Value
+                if (A[a] < B[b])
+                    a++;
+
+                else
+                    b++;
+            }
+
+            // return final sma result
+            return result;
+        }
 
 
     }
